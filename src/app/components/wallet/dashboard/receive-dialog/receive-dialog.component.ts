56456051f5b05dc8b1c49515e0de5616c67c759f
David Klie <mangok543@gmail.com>
2017-07-18 14:40:26 -0400

    Decrypted Wallet Observable added to recieve-dialog subcomponent.

--- src/app/components/wallet/dashboard/receive-dialog/receive-dialog.component.ts
+++ src/app/components/wallet/dashboard/receive-dialog/receive-dialog.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { MdSnackBar, MdDialog } from '@angular/material';
 import { WalletService } from '../../../../shared/services/wallet.service';
+import {Subscription} from 'rxjs/Subscription';
+
 
 @Component({
     selector: 'app-receive-dialog',
@@ -8,12 +10,18 @@ import { WalletService } from '../../../../shared/services/wallet.service';
     styleUrls: ['./receive-dialog.component.css']
 })
 export class ReceiveDialogComponent implements OnInit {
-
+    public decryptedWallet: string;
+    private decryptedWallet$$: Subscription;
     constructor(private _dialog: MdDialog,
                 private _snackbar: MdSnackBar,
                 public _walletService: WalletService) { }
 
     ngOnInit() {
+        this.decryptedWallet$$ = this._walletService.decryptedWallet$
+            .subscribe(walletObject => {
+                console.log(walletObject);
+                this.decryptedWallet = walletObject;
+            });
     }
 
     showMessage(message: string): void {
