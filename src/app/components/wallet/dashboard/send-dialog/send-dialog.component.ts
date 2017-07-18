c38e19f090456b3c6061d338b31fbab3ed2a1997
David Klie <mangok543@gmail.com>
2017-07-18 14:53:19 -0400

    Send dialog component added Decrypted wallet subscription.

--- src/app/components/wallet/dashboard/send-dialog/send-dialog.component.ts
+++ src/app/components/wallet/dashboard/send-dialog/send-dialog.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Subscription} from 'rxjs/Subscription';
+import { WalletService } from '../../../../shared/services/wallet.service';
 @Component({
   selector: 'app-send-dialog',
   templateUrl: './send-dialog.component.html',
   styleUrls: ['./send-dialog.component.css']
 })
 export class SendDialogComponent implements OnInit {
-
-  constructor() { }
+  
+  public decryptedWallet: string;
+  private decryptedWallet$$: Subscription;
+  constructor(private _walletService: WalletService) { }
 
   ngOnInit() {
+    this.decryptedWallet$$ = this._walletService.decryptedWallet$
+            .subscribe(walletObject => {
+                console.log(walletObject);
+                this.decryptedWallet = walletObject;
+            });
   }
 
 }
